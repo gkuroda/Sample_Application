9397f211bb36feaf18d6bebca9a54f435503335a
gkuroda <gkuroda@netprotections.co.jp>
2022-03-15 16:10:20 +0900

    DataBinding連携の実装

--- app/src/main/java/app/gkuroda/sampleapplication/ui/MainActivity.kt
+++ app/src/main/java/app/gkuroda/sampleapplication/ui/MainActivity.kt
@@ -2,8 +2,10 @@ package app.gkuroda.sampleapplication.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import app.gkuroda.sampleapplication.R
 import app.gkuroda.sampleapplication.dagger.viewModel.ViewModelFactory
+import app.gkuroda.sampleapplication.databinding.ActivityMainBinding
 import dagger.android.AndroidInjection
 import javax.inject.Inject
 
@@ -14,15 +16,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainActivityViewModel
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         // @Inject で指定している変数に値を注入する
         AndroidInjection.inject(this)
 
+        // bindingの初期化
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // LiveDataを使う際はlifecycleOwnerを指定してあげないといけない
+        binding.lifecycleOwner = this
+
+        //ViewModelの初期化
         viewModel = viewModelFactory.get(this)
 
-
+        // MVVMにおける ViewとViewModelを結びつける工程
+        // bindingにViewModelを渡す
+        binding.viewModel = viewModel
     }
 }
--- app/src/main/java/app/gkuroda/sampleapplication/ui/MainActivityViewModel.kt
+++ app/src/main/java/app/gkuroda/sampleapplication/ui/MainActivityViewModel.kt
@@ -1,6 +1,28 @@
 package app.gkuroda.sampleapplication.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import app.gkuroda.sampleapplication.dagger.model.UserSettingModel
+import javax.inject.Inject
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel @Inject constructor(
+    private val userSettingModel: UserSettingModel
+) : ViewModel() {
+
+    val isSwitchEnable: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
+
+    // classが初期化された段階で実行される
+    init {
+        loadSwitchStatus()
+    }
+
+    fun loadSwitchStatus() {
+        isSwitchEnable.postValue(userSettingModel.loadSwitchStatus())
+    }
+
+    fun saveSwitchStatus(isEnable: Boolean) {
+        userSettingModel.saveSwitchStatus(isEnable)
+    }
 }
